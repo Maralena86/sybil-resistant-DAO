f5937dce3bf8311a8b7cee5d1bffce94cccb4d93
pgrandne <perrin.grandne@protonmail.com>
2023-07-22 14:04:22 +0200

    Adding gitcoin score

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,55 +1,40 @@
-
 import Image from 'next/image'
 import Link from 'next/link'
 export function Card() {
   return (
-
-        <div className="relative card py-12 px-12 rounded-3xl w-82 my-4 shadow-xl">
-            <div className="  card-icon flex items-center absolute rounded-full py-4 px-4 shadow-xl left-4 -top-6">
-               
-            <Image
-              src="/gitcoinco.png"
-              width={30}
-              height={30}
-              alt="Gitcoin"
-            />
-           
-            </div>
-            <div className="  card-icon flex items-center absolute rounded-full py-4 px-4 shadow-xl right-4 -top-6">
-            <Image
-              src="/sismo.png"
-              width={30}
-              height={30}
-              alt="Gitcoin"
-            />
-            </div>
-            <div className="mt-8 text-center">
-                <p className="text-xl font-semibold my-2">Join the  Sybil Resitant DAO</p>
-                <div className='py-6'>
-                  <div className="flex space-x-2   ">
-                    
-                      
-                      <p>Score</p> 
-                  </div>
-                  <div className="flex text-gray-400">
-                      
-                      <Link href="#">There is a link </Link>
-                        
-                  </div>
-                </div>
-                
-
-                <div className="flex justify-between">
-                    
-                    
-                  <div className="text-base card-button font-semibold">
-                        <button>Mint </button>
-                        
-                    </div>
-                </div>
-            </div>
+    <div className="relative card py-6 px-6 rounded-3xl w-82 my-4 shadow-xl">
+      <div className="  card-icon flex items-center absolute rounded-full py-4 px-4 shadow-xl left-4 -top-6">
+        <Image
+          src="/gitcoinco.png"
+          width={30}
+          height={30}
+          alt="Gitcoin"
+        />
+      </div>
+      <div className="  card-icon flex items-center absolute rounded-full py-4 px-4 shadow-xl right-4 -top-6">
+        <Image
+          src="/sismo.png"
+          width={30}
+          height={30}
+          alt="Gitcoin"
+        />
+      </div>
+      <div className="mt-8 text-center">
+        <p className="text-xl font-semibold my-2">Join the  Sybil Resitant DAO</p>
+        <div className='py-6'>
+          <div className="flex space-x-2   ">
+            <p>Score</p>
+          </div>
+          <div className="flex text-gray-400">
+            <Link href="#">There is a link </Link>
+          </div>
         </div>
-
-       
+        <div className="flex justify-between">
+          <div className="text-base card-button font-semibold">
+            <button>Mint </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,68 @@
-"use client"
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal, Web3Button } from '@web3modal/react'
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon, goerli } from 'wagmi/chains'
-import {Card} from './components/Card'
-import Image from 'next/image'
+"use client";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal, Web3Button } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon, goerli } from "wagmi/chains";
+import { Card } from "./components/Card";
+import Image from "next/image";
+import { Footer } from "./Footer";
 
-const chains = [arbitrum, mainnet, polygon, goerli]
-const projectId = process.env.NEXT_PUBLIC_PROJECT_ID!
+const chains = [arbitrum, mainnet, polygon, goerli];
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID!;
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, chains }),
-  publicClient
-})
-const ethereumClient = new EthereumClient(wagmiConfig, chains)
-
-
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  p-6">
-      <div className='flex justify-between'>
-        <Image
-        src="/favicon.ico"
-        width={40}
-        height={40}
-        alt="logo"
-        />
-        <div className=''>
-          <WagmiConfig config={wagmiConfig} >
-            <Web3Button />
-          </WagmiConfig>
-          <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-        </div>
-      </div>
-      <div className="flex items-center justify-center p-24">
-        <div className="">
-          <Card/>
-        </div>
-      </div>
+    <main>
+      <WagmiConfig config={wagmiConfig}>
+        <section className="flex flex-col  p-6">
+          <div className="flex justify-between">
+            <Image src="/favicon.ico" width={40} height={40} alt="logo" />
+            <div className="">
+              <Web3Button />
+            </div>
+          </div>
+          <div className="flex items-center flex-col p-20">
+            <div className="">
+              <Card />
+            </div>
+            <div className="pt-24 flex gap-12">
+              <Image
+                src="/shield-gold.png"
+                width={200}
+                height={200}
+                alt="logo"
+              />
+              <Image
+                src="/shield-argent.png"
+                width={200}
+                height={200}
+                alt="logo"
+              />
+
+              <Image
+                src="/shield-bronze.png"
+                width={200}
+                height={200}
+                alt="logo"
+              />
+            </div>
+          </div>
+
+        </section>
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </main>
-  )
+  );
 }
