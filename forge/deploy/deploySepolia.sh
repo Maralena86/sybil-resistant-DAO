bb51713689d7e8425989fdd397b8a8272d88f4e2
0xyanc <yannick.chi@gmail.com>
2023-07-22 16:33:04 +0200

    Add EAS and Sepolia

---
+++ forge/deploy/deploySepolia.sh
@@ -0,0 +1,3 @@
+forge script script/deploySepolia.s.sol:Deploy --rpc-url "https://eth-sepolia.g.alchemy.com/v2/$INFURA_KEY" \
+--private-key $WALLET_PRIVATE_KEY \
+--broadcast -vvvv
--- forge/deploy/script/deploySepolia.s.sol
+++ forge/deploy/script/deploySepolia.s.sol
@@ -29,8 +29,8 @@ contract Deploy is Script {
           0,
           10000,
           10000,
-          0x610c92c70Eb55dFeAFe8970513D13771Da79f2e0,
-          0x0000000000000000000000000000000000000000
+          0x78f83b36468bFf785046974e21A1449b47FD7e74,
+          0xC2679fBD37d54388Ce493F1DB75320D236e1815e
         );
         
         console.log("NFT Factory deployed: ", address(factory));
