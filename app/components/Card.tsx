b7d701be5bc2c2e6dbffab7c976c58fd8b791590
Maralena86 <mari16cobain@msn.com>
2023-07-22 08:13:10 +0200

    front changes

---
+++ app/components/Card.tsx
@@ -0,0 +1,46 @@
+
+import Image from 'next/image'
+import Link from 'next/link'
+export function Card() {
+  return (
+
+        <div className="relative card py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+            <div className=" text-white  card-icon flex items-center absolute rounded-full py-4 px-4 shadow-xl left-4 -top-6">
+               
+            <Image
+              src="/gitcoinco.png"
+              width={30}
+              height={30}
+              alt="Gitcoin"
+            />
+            </div>
+            <div className="mt-8">
+                <p className="text-xl font-semibold my-2">Gitcoin</p>
+                <div className='py-6'>
+                  <div className="flex space-x-2   ">
+                    
+                      
+                      <p>Score</p> 
+                  </div>
+                  <div className="flex text-gray-400">
+                      
+                      <Link href="#">There's a link </Link>
+                        
+                  </div>
+                </div>
+                
+
+                <div className="flex justify-between">
+                    
+                    
+                  <div className="text-base card-button font-semibold">
+                        <button>Verify </button>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+       
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal, Web3Button } from '@web3modal/react'
 import { configureChains, createConfig, WagmiConfig } from 'wagmi'
 import { arbitrum, mainnet, polygon, goerli } from 'wagmi/chains'
+import {Card} from './components/Card'
+import Image from 'next/image'
 
 const chains = [arbitrum, mainnet, polygon, goerli]
 const projectId = '91c0353f17b535b32824d5df3a07418a'
@@ -15,13 +17,31 @@ const wagmiConfig = createConfig({
 })
 const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
+
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-6">
-      <WagmiConfig config={wagmiConfig} >
-        <Web3Button/>
-      </WagmiConfig>
+    <main className="flex min-h-screen flex-col  p-6">
+      <div className='flex justify-between'>
+        
+        
+        <Image
+         src="/favicon.ico"
+         width={35}
+         height={20}
+         alt="Gitcoin"/>
+         <WagmiConfig config={wagmiConfig} >
+          <Web3Button />
+        </WagmiConfig>
+        
+      </div>
+      
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      <div className="flex items-center justify-center p-24">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
+          <Card/>
+        </div>
+      </div>
     </main>
   )
 }
